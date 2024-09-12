5c0852caa03d0feb387541f93ea21bcff4d66543
PAhlback <pontus.ahlback93@gmail.com>
2024-09-12 15:00:57 +0200

    Add project files.

---
+++ RestaurantBookingSystemMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantBookingSystemMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
